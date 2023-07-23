7ab861d117b006b538962df053391df9ad72c8b6
Luis Pedro Coelho <luis@luispedro.org>
2023-07-23 12:37:51 +0200

    BUG Fix timetable
    Conversion script now reloads from Google sheets (ideally would be
    done at deployment, but this is already better than before)
    
    closes #1

--- convert-to-json.py
+++ convert-to-json.py
@@ -1,2 +1,19 @@
 import pandas as pd
-pd.read_table('ISMB_ECCB 2023 - All sessions.tsv').to_json(open('ISMB_ECCB_2023_All_sessions.json', 'wt'), orient='records', force_ascii=False)
+COLS = ['Session',
+ 'Room',
+ 'Weekday',
+ 'Date',
+ 'Timespan',
+ 'Format',
+ 'Type',
+ 'Speaker',
+ 'Title',
+ 'Abstract']
+
+URL = 'https://docs.google.com/spreadsheets/d/169W9o9nGMqYQ5Ojplh1jd3WkEHNgWk4EwREkWo6fD6k/gviz/tq?tqx=out:csv'
+timetable = pd.read_csv(URL, usecols=COLS)
+with open('ISMB_ECCB_2023_All_sessions.json', 'wt') as out:
+    timetable.to_json(
+            out,
+            orient='records',
+            force_ascii=False)
