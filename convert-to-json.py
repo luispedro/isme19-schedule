8d7d5f84b200208a0c292f43eca28d1d51dc6ebb
Luis Pedro Coelho <luis@luispedro.org>
2023-07-26 10:43:24 +0200

    BUG Fix encodings of title/speaker names

--- convert-to-json.py
+++ convert-to-json.py
@@ -12,6 +12,20 @@ COLS = ['Session',
 
 URL = 'https://docs.google.com/spreadsheets/d/169W9o9nGMqYQ5Ojplh1jd3WkEHNgWk4EwREkWo6fD6k/gviz/tq?tqx=out:csv'
 timetable = pd.read_csv(URL, usecols=COLS)
+
+def fix_encoding1(s):
+    try:
+        return s.encode('macroman').decode('utf-8')
+    except:
+        return s
+def fix_encodings(talk):
+    if talk.Session == "Function":
+        talk.Title = fix_encoding1(talk.Title)
+        talk.Speaker = fix_encoding1(talk.Speaker)
+    return talk
+
+timetable = timetable.apply(fix_encodings, axis=1)
+
 with open('ISMB_ECCB_2023_All_sessions.json', 'wt') as out:
     timetable.to_json(
             out,
