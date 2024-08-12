288c28b437d40af497df3cc9821dcafa54c52b32
Luis Pedro Coelho <luis@luispedro.org>
2024-08-12 14:37:19 +1000

    ENH Parse poster information

--- get-schedule.py
+++ get-schedule.py
@@ -10,6 +10,9 @@ BASE_URL = 'https://www.eventure-online.com/eventure/login.form?U7db3f155-cc7a-4
 URL = 'https://www.eventure-online.com/eventure/public/ajaxGetPublicSessionList.form?cuuid=7db3f155-cc7a-482b-ac36-191cd4f21cd7'
 SESSION_URL_PAT = 'https://www.eventure-online.com/eventure/public/ajaxViewPublicSession.form?cuuid=7db3f155-cc7a-482b-ac36-191cd4f21cd7&sessionUuid={uuid}'
 
+POSTER_LIST_URL = 'https://www.eventure-online.com/eventure/public/ajaxGetPublicPosterList.form?cuuid=7db3f155-cc7a-482b-ac36-191cd4f21cd7'
+POSTER_DETAILS_URL_PAT = 'https://www.eventure-online.com/eventure/public/ajaxViewPublicSession.form?cuuid=7db3f155-cc7a-482b-ac36-191cd4f21cd7&posterUuid={uuid}'
+
 
 @TaskGenerator
 def get_session_information():
@@ -67,8 +70,76 @@ def parse_session_info(session_info):
 def save_session_info(session_info):
     json.dump(session_info, open('src/ISME19_all_sessions.json', 'wt'))
 
+@TaskGenerator
+def get_poster_session_information():
+    r = requests.get(BASE_URL)
+    cookies = r.cookies
+    r = requests.get(POSTER_LIST_URL, cookies=cookies)
+    p = bs4.BeautifulSoup(r.text, 'html.parser')
+    poster_sessions = []
+    for t in p.find('table').find_all('tr')[1:]:
+        uuid = t.get('data-uuid')
+        toks = t.find_all('td')
+        code = toks[0].text
+        title = toks[1].text
+        stype = toks[2].text
+        location = toks[3].text
+        date, time = toks[4].strings
+
+        s_url = POSTER_DETAILS_URL_PAT.format(uuid=uuid)
+        nr = requests.get(s_url, cookies=cookies)
+        np = bs4.BeautifulSoup(nr.text, 'html.parser')
+        poster_sessions.append({
+            'uuid': uuid,
+            'code': code,
+            'title': title,
+            'type': stype,
+            'location': location,
+            'date': str(date), # Otherwise we get a NavigableString which is not picklable
+            'time': str(time),
+            'session-info': np
+            })
+        sleep(1 + 2*random())
+        print(f'Got poster session {title} (total of {len(poster_sessions)} sessions)')
+    return poster_sessions
+
+@TaskGenerator
+def parse_poster_info(posters_info):
+    reparsed = []
+    for p in posters_info:
+        p = p.copy()
+        posters = []
+        for tr in p['session-info'].find_all('table')[2].find_all('tr')[1:]:
+            elems = list(tr.strings)
+            title = ''.join(elems[1:-1])
+            author = str(elems[-1])
+            posters.append((title, author))
+        p['posters'] = posters
+        del p['session-info']
+        reparsed.append(p)
+    return reparsed
+
+
+@TaskGenerator
+def format_save_posters(parsed_posters):
+    posters = []
+    for s in parsed_posters:
+        for title,author in s['posters']:
+            posters.append({
+                'title': title,
+                'author': author,
+                'location': s['location'],
+                'date': s['date'],
+                'time': s['time'],
+                'session_title': s['title'],
+                })
+    json.dump(posters, open('src/ISME19_all_posters.json', 'wt'))
+    return posters
+
 
 session_info = get_session_information()
 parsed_info = parse_session_info(session_info)
 save_session_info(parsed_info)
-
+posters_info = get_poster_session_information()
+parsed_posters = parse_poster_info(posters_info)
+format_save_posters(parsed_posters)
